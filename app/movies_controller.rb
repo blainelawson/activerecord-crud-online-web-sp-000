f3540a33fdebfaceae60b71c341de7e955451018
Blaine Lawson <blaine.lawson@gmail.com>
2019-04-29 16:14:08 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -35,8 +35,8 @@ def can_be_created_in_a_block(title: 'Home Alone', release_date: 1990)
 
   Movie.create do |m|
     # binding.pry
-    self.title = title
-    self.release_date = release_date
+    m.title = title
+    m.release_date = release_date
   end
 end
 
