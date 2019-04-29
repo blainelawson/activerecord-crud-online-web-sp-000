b1b5ebc3597eb8ffac7e1242b4aeff74eb20ec34
Blaine Lawson <blaine.lawson@gmail.com>
2019-04-29 16:20:09 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -44,11 +44,11 @@ def can_get_the_first_item_in_the_database
 end
 
 def can_get_the_last_item_in_the_database
-  Movie.last.id
+  Movie.last
 end
 
 def can_get_size_of_the_database
-  Movie.size
+  Movie.last.id
 end
 
 def can_find_the_first_item_from_the_database_using_id
